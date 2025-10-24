ecb5c415f6364721db64732b805e02e1ef0fe9c3
marfanr <mohammadarfan45@gmail.com>
2025-10-24 07:02:19 +0700

    refactor: enhance metadata structures and improve file handling in Builder

--- include/metadata.hh
+++ include/metadata.hh
@@ -2,20 +2,39 @@
 #define __METADATA_HH__
 
 #include <cstdint>
-struct  metadata_file {
-    uint64_t next_offset;
-    uint32_t metadata_length;
-    uint32_t size;
-    uint16_t nama_file_len;
-    // char name[nama_file_len]
+
+
+struct metadata_string {
+    uint16_t length;
+    char *data; 
+};
+
+struct metadata_list {
+    uint16_t count;
+    struct metadata_string *items;
 };
 
 struct metadata_header {
     uint32_t magic;
     uint16_t version;
     uint32_t header_len;
-    uint16_t nama_module_len;
-    // char nama_module[nama_module_len]
+
+    struct metadata_string nama_module;
+    struct metadata_string description;
+    struct metadata_string license;
+    struct metadata_string version_str;
+    struct metadata_string author;
+    struct metadata_string main_file;
+
+    struct metadata_list capability;
+};
+
+
+struct  metadata_file {
+    uint64_t next_offset;
+    uint32_t metadata_length;
+    uint32_t size;
+    struct metadata_string nama_file;
 };
 
 #endif // __METADATA__HH__
--- src/builder.cc
+++ src/builder.cc
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <variant>
 #include <vector>
 
 #define VOXMO_MAGIC 0x564f584d
@@ -30,6 +31,34 @@ std::vector<std::string> get_lines(std::vector<char> data) {
   return lines;
 }
 
+static void set_metadata_string(metadata_string &dest, const std::string &src) {
+  dest.length = static_cast<uint16_t>(src.length());
+  dest.data = new char[dest.length];
+  std::copy(src.begin(), src.end(), dest.data);
+}
+
+void set_metadata_list(struct metadata_list &dest,
+                       const std::vector<std::string> &src) {
+  dest.count = static_cast<uint16_t>(src.size());
+  dest.items = new metadata_string[dest.count];
+
+  for (uint16_t i = 0; i < dest.count; ++i) {
+    const auto &s = src[i];
+    dest.items[i].length = static_cast<uint16_t>(s.length());
+    dest.items[i].data = new char[dest.items[i].length];
+    std::copy(s.begin(), s.end(), dest.items[i].data);
+  }
+}
+
+void free_metadata_list(metadata_list &list) {
+  for (uint16_t i = 0; i < list.count; ++i) {
+    delete[] list.items[i].data;
+  }
+  delete[] list.items;
+  list.items = nullptr;
+  list.count = 0;
+}
+
 void Builder::build(std::string filename) {
 
   // find manifest
@@ -48,49 +77,69 @@ void Builder::build(std::string filename) {
     std::cerr << "error manifest not found" << std::endl;
     return;
   }
+
   std::vector<std::string> lines = get_lines(manifest->data);
   Manifest m(std::move(lines));
+  const std::vector<std::string> required_fields = {
+      "name", "version", "author", "description", "license", "main"};
 
-  // creating output file
-  std::ofstream out(filename, std::ios::out | std::ios::binary);
-  if (!out) {
-    std::cerr << "Failed to open file " << filename << "\n";
-    return;
+  for (const auto &field : required_fields) {
+    if (std::holds_alternative<std::vector<std::string>>(m[field])) {
+      std::cerr << "error: module must have " << field << std::endl;
+      return;
+    }
   }
+
+  std::ofstream out(filename, std::ios::binary | std::ios::out);
 
   // creating metadata
   metadata_header header;
   header.magic = VOXMO_MAGIC;
   header.version = 1;
   header.header_len = 0;
-  header.nama_module_len = manifest->path.length();
-  header.header_len = sizeof(header) + header.nama_module_len;
+
+  std::string name = std::get<std::string>(m["name"]);
+  set_metadata_string(header.nama_module, name);
+  header.header_len += header.nama_module.length;
+  set_metadata_string(header.description,
+                      std::get<std::string>(m["description"]));
+  header.header_len += header.description.length;
+  set_metadata_string(header.license, std::get<std::string>(m["license"]));
+  header.header_len += header.license.length;
+  set_metadata_string(header.version_str, std::get<std::string>(m["version"]));
+  header.header_len += header.version_str.length;
+  set_metadata_string(header.author, std::get<std::string>(m["author"]));
+  header.header_len += header.author.length;
+  set_metadata_string(header.main_file, std::get<std::string>(m["main"]));
+  header.header_len += header.main_file.length;
+
+  if (std::holds_alternative<std::vector<std::string>>(m["capability"])) {
+    set_metadata_list(header.capability,
+                      std::get<std::vector<std::string>>(m["capability"]));
+    header.header_len += header.capability.count * sizeof(metadata_string);
+  } else {
+    set_metadata_list(header.capability, std::vector<std::string>());
+    header.header_len += sizeof(metadata_list);
+  }
+
+  // write header
   out.write((char *)&header, sizeof(header));
 
-  char *nama_module = new char[header.nama_module_len];
-  std::copy(manifest->path.begin(), manifest->path.end(), nama_module);
-  out.write(nama_module, header.nama_module_len);
-  delete[] nama_module;
-
+  // add file except metadata
   for (const auto &f : loader->get_files()) {
     std::filesystem::path p(f->path);
     if (p.filename() == "manifest.yaml") {
       continue;
     }
 
+
     metadata_file mf;
     mf.size = f->data.size();
-    mf.nama_file_len = mf.nama_file_len =
-        static_cast<uint16_t>(std::string(p.filename()).length());
-    mf.next_offset = sizeof(header) + header.nama_module_len + sizeof(mf) +
-                     mf.nama_file_len + f->data.size();
-    mf.metadata_length = sizeof(mf) + mf.nama_file_len;
+    set_metadata_string(mf.nama_file, p.filename().string());
+    mf.metadata_length = sizeof(mf) + mf.nama_file.length;
+    mf.next_offset = header.header_len + mf.metadata_length + mf.size;
+
     out.write((char *)&mf, sizeof(mf));
-
-    std::string name_str = p.filename().string();
-    out.write(name_str.data(), name_str.size());
-    mf.nama_file_len = static_cast<uint16_t>(name_str.size());
-
     out.write(f->data.data(), f->data.size());
   }
 }
